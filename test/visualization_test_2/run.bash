c22e44a6f2c281613c43b2c9c449d0fba5fca373
Alan Heirich <aheirich@slac.stanford.edu>
2019-07-18 15:20:36 -0700

    all tests pass, use run.bash to run them on ubuntu

--- test/visualization_test_2/run.bash
+++ test/visualization_test_2/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LEGION_DIR/language/hdf/install/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDF_ROOT/lib
 mpirun -n 1 -npernode 4 ./visualization_test_2 -ll:cpu 4 -ll:csize 2048 
--- test/visualization_test_3/run.bash
+++ test/visualization_test_3/run.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HDf_ROOT/lib
 mpirun -n 1 -npernode 4 ./visualization_test_3 -ll:cpu 4 -ll:csize 2048 
