24395f24011de21048bacc0c98b820977e787995
Alan Heirich <aheirich@stanford.edu>
2019-10-22 21:45:11 +0200

    fix run script for visualization_1

--- examples/visualization_1/scripts/run_piz_daint_4_nodes.bash
+++ examples/visualization_1/scripts/run_piz_daint_4_nodes.bash
@@ -17,4 +17,4 @@ source ~/image-compositor/setup.bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LEGION_DIR/bindings/regent/
 
 
-srun -C gpu -N 4 $(IMAGE_COMPOSITOR_DIR)/examples/visualization_1/visualization_1 -ll:cpu 16 -ll:csize 8192
+srun -C gpu -N 4 $IMAGE_COMPOSITOR_DIR/examples/visualization_1/visualization_1 -ll:cpu 16 -ll:csize 8192
