808386647640ddceef795625a5f6f2771f310ebb
Qi Wu <wilson.over.cloud@gmail.com>
2022-09-15 15:54:30 -0700

    add script to select gpu

--- examples/visualization_3/run-optix7.sh
+++ examples/visualization_3/run-optix7.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 export REALM_FREEZE_ON_ERROR=1
 export REALM_USE_CUDART_HIJACK=0
-# mpirun -np 2 visualization_3 -pipeline ../volume.py -logfile run_%.log
- 
-make -j && mpirun -np 4 visualization_3 \
-    -pipeline volume.py -logfile run_%.logls -level mapper=2,inst=1 \
-    -ll:gpu 1
+
+cd /home/qwu/image-compositor/examples/visualization_3/build-cuda
+CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK ./visualization_3 -pipeline volume.py \
+    -logfile run_%.logls -level mapper=2,inst=1 -ll:gpu 1
--- examples/visualization_3/run-ospray.sh
+++ examples/visualization_3/run-ospray.sh
@@ -1,6 +1,9 @@
 export REALM_FREEZE_ON_ERROR=1
 export REALM_USE_CUDART_HIJACK=0
 # mpirun -np 2 visualization_3 -pipeline ../volume.py -logfile run_%.log
- 
-make -j && mpirun -np 2 visualization_3 \
-    -pipeline volume.py -logfile run_%.logls -level mapper=2,inst=1 \
+# make -j && mpirun -np 2 visualization_3 \
+#     -pipeline volume.py -logfile run_%.logls -level mapper=2,inst=1 \
+
+cd /home/qwu/image-compositor/examples/visualization_3/build-cuda
+CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK ./visualization_3 -pipeline volume.py \
+    -logfile run_%.logls -level mapper=2,inst=1
