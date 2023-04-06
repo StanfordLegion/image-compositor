9a044c2d4b5b09b57e67ca7e2ab09699d6206a54
Seshu Yamajala <syamajala@gmail.com>
2023-04-06 11:12:03 -0400

    Add libpng.

--- compile.sh
+++ compile.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+export LEGION_DIR=$(pwd)/../legion
 mkdir -p deps
 cd deps
 
 DEPS=$(pwd)
 
-wget https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
-tar xvzf libpng-1.6.39.tar.gz
-cd libpng-1.6.39
-./configure --prefix=$DEPS/libpng
-make -j
-make PREFIX=$DEPS/libpng install
+if [ ! -d "$DEPS/libpng" ]; then
+   wget https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+   tar xvzf libpng-1.6.39.tar.gz
+   cd libpng-1.6.39
+   ./configure --prefix=$DEPS/libpng
+   make -j
+   make PREFIX=$DEPS/libpng install
+   cd ..
+fi
 
-wget https://github.com/ospray/ospray/releases/download/v2.9.0/ospray-2.9.0.x86_64.linux.tar.gz
-tar xzvf ospray-2.9.0.x86_64.linux.tar.gz
+if [ ! -d "ospray-2.9.0.x86_64.linux" ]; then
+   wget https://github.com/ospray/ospray/releases/download/v2.9.0/ospray-2.9.0.x86_64.linux.tar.gz
+   tar xzvf ospray-2.9.0.x86_64.linux.tar.gz
+fi
 
-wget https://github.com/oneapi-src/oneTBB/releases/download/v2021.4.0/oneapi-tbb-2021.4.0-lin.tgz
-tar xzvf oneapi-tbb-2021.4.0-lin.tgz
+if [ ! -d "oneapi-tbb-2021.4.0" ]; then
+   wget https://github.com/oneapi-src/oneTBB/releases/download/v2021.4.0/oneapi-tbb-2021.4.0-lin.tgz
+   tar xzvf oneapi-tbb-2021.4.0-lin.tgz
+fi
 
-mkdir open-volume-renderer
-cd open-volume-renderer
-tar xzvf ../../open-volume-renderer.tar.gz
-cd ..
+if [ ! -d "open-volume-renderer" ]; then
+   mkdir open-volume-renderer
+   cd open-volume-renderer
+   tar xzvf ../../open-volume-renderer.tar.gz
+   cd ..
+fi
 
 cd ..
 
@@ -32,9 +42,9 @@ pwd
 
 # note: BUILD_WITH_CUDA means whether Legion has CUDA with it or not
 #       OVR_BUILD_DEVICE_OPTIX7 means whether we want to enable the OptiX renderer
-VERBOSE=1 cmake ../ \
-    -DBUILD_WITH_CUDA=ON -DOVR_BUILD_DEVICE_OPTIX7=OFF \
+PKG_CONFIG_PATH=$DEPS/libpng/lib/pkgconfig:$PKG_CONFIG_PATH VERBOSE=0 cmake ../ \
+    -DBUILD_WITH_CUDA=OFF -DOVR_BUILD_DEVICE_OPTIX7=OFF \
     -Dospray_DIR=${DEPS}/ospray-2.9.0.x86_64.linux/lib/cmake/ospray-2.9.0/ \
-    -DTBB_DIR=${DEPS}/oneapi-tbb-2021.4.0/lib/cmake/tbb
+    -DTBB_DIR=${DEPS}/oneapi-tbb-2021.4.0/lib/cmake/tbb \
 
-VERBOSE=1 cmake --build . --config Debug --parallel 16
+VERBOSE=0 cmake --build . --config Debug --parallel 16
